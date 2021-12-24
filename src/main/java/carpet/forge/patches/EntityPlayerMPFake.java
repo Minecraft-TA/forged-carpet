1d5501efa21dc48de79f41efdde3ff1ba97ef303
tth05 <36999320+tth05@users.noreply.github.com>
2021-12-24 23:08:03 +0100

    Performance fixes

--- src/main/java/carpet/forge/patches/EntityPlayerMPFake.java
+++ src/main/java/carpet/forge/patches/EntityPlayerMPFake.java
@@ -2,7 +2,10 @@ package carpet.forge.patches;
 
 import carpet.forge.fakes.IEntityPlayerMP;
 import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.inventory.Container;
 import net.minecraft.network.EnumPacketDirection;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketEntityHeadLook;
@@ -18,27 +21,39 @@ import net.minecraft.world.GameType;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.handshake.NetworkDispatcher;
 
-@SuppressWarnings("EntityConstructor")
-public class EntityPlayerMPFake extends EntityPlayerMP
-{
+import javax.annotation.Nonnull;
+
+public class EntityPlayerMPFake extends EntityPlayerMP {
+
+    private static final Container DUMMY_CONTAINER = new Container() {
+        @Override
+        public void detectAndSendChanges() {
+            //NO OP - Performance
+        }
+
+        @Override
+        public boolean canInteractWith(EntityPlayer playerIn) {
+            return true;
+        }
+    };
+
     private double lastReportedPosX;
     private double lastReportedPosY;
     private double lastReportedPosZ;
-    
+
     private double setX;
     private double setY;
     private double setZ;
     private float setYaw;
     private float setPitch;
-    
-    public static EntityPlayerMPFake createFake(String username, MinecraftServer server, double x, double y, double z, double yaw, double pitch, int dimension, int gamemode)
-    {
+
+    public static EntityPlayerMPFake createFake(String username, MinecraftServer server, double x, double y, double z, double yaw, double pitch, int dimension, int gamemode) {
         WorldServer worldIn = server.getWorld(dimension);
         PlayerInteractionManager interactionManagerIn = new PlayerInteractionManager(worldIn);
         GameProfile gameprofile = server.getPlayerProfileCache().getGameProfileForUsername(username);
         gameprofile = fixSkin(gameprofile);
         EntityPlayerMPFake instance = new EntityPlayerMPFake(server, worldIn, gameprofile, interactionManagerIn);
-        instance.setSetPosition(x, y, z, (float)yaw, (float)pitch);
+        instance.setSetPosition(x, y, z, (float) yaw, (float) pitch);
         PlayerList playerList = server.getPlayerList();
         NetworkManager manager = new NetworkManagerFake(EnumPacketDirection.CLIENTBOUND);
         manager.channel().attr(NetworkDispatcher.FML_DISPATCHER).getAndSet(new NetworkDispatcher(manager, playerList));
@@ -52,22 +67,21 @@ public class EntityPlayerMPFake extends EntityPlayerMP
             worldIn.spawnEntity(instance);
             instance.setWorld(worldIn);
             server.getPlayerList().preparePlayer(instance, old_world);
-            instance.connection.setPlayerLocation(x, y, z, (float)yaw, (float)pitch);
+            instance.connection.setPlayerLocation(x, y, z, (float) yaw, (float) pitch);
             instance.interactionManager.setWorld(worldIn);
         }
         instance.setHealth(20.0F);
         instance.isDead = false;
         instance.stepHeight = 0.6F;
         interactionManagerIn.setGameType(GameType.getByID(gamemode));
-        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityHeadLook(instance, (byte)(instance.rotationYawHead * 256 / 360) ),instance.dimension);
-        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityTeleport(instance),instance.dimension);
+        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityHeadLook(instance, (byte) (instance.rotationYawHead * 256 / 360)), instance.dimension);
+        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityTeleport(instance), instance.dimension);
         server.getPlayerList().serverUpdateMovingPlayer(instance);
         instance.dataManager.set(PLAYER_MODEL_FLAG, (byte) 0x7f); // show all model layers (incl. capes)
         return instance;
     }
-    
-    public static EntityPlayerMPFake createShadow(MinecraftServer server, EntityPlayerMP player)
-    {
+
+    public static EntityPlayerMPFake createShadow(MinecraftServer server, EntityPlayerMP player) {
         player.getServer().getPlayerList().playerLoggedOut(player);
         player.connection.disconnect(new TextComponentTranslation("multiplayer.disconnect.duplicate_login"));
         WorldServer worldIn = server.getWorld(player.dimension);
@@ -80,81 +94,84 @@ public class EntityPlayerMPFake extends EntityPlayerMP
         NetworkManager manager = new NetworkManagerFake(EnumPacketDirection.CLIENTBOUND);
         manager.channel().attr(NetworkDispatcher.FML_DISPATCHER).getAndSet(new NetworkDispatcher(manager, playerList));
         playerList.initializeConnectionToPlayer(manager, playerShadow, playerShadow.connection);
-        
+
         playerShadow.setHealth(player.getHealth());
-        playerShadow.connection.setPlayerLocation(player.posX, player.posY,player.posZ, player.rotationYaw, player.rotationPitch);
+        playerShadow.connection.setPlayerLocation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch);
         interactionManagerIn.setGameType(player.interactionManager.getGameType());
         //noinspection ConstantConditions
         ((IEntityPlayerMP) playerShadow).getActionPack().copyFrom(((IEntityPlayerMP) player).getActionPack());
         playerShadow.stepHeight = 0.6F;
-        
-        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityHeadLook(playerShadow, (byte)(player.rotationYawHead * 256 / 360) ),playerShadow.dimension);
+
+        server.getPlayerList().sendPacketToAllPlayersInDimension(new SPacketEntityHeadLook(playerShadow, (byte) (player.rotationYawHead * 256 / 360)), playerShadow.dimension);
         server.getPlayerList().sendPacketToAllPlayers(new SPacketPlayerListItem(SPacketPlayerListItem.Action.ADD_PLAYER, playerShadow));
         server.getPlayerList().serverUpdateMovingPlayer(playerShadow);
         return playerShadow;
     }
-    
-    private EntityPlayerMPFake(MinecraftServer server, WorldServer worldIn, GameProfile profile, PlayerInteractionManager interactionManagerIn)
-    {
+
+    private EntityPlayerMPFake(MinecraftServer server, WorldServer worldIn, GameProfile profile, PlayerInteractionManager interactionManagerIn) {
         super(server, worldIn, profile, interactionManagerIn);
     }
-    
-    private static GameProfile fixSkin(GameProfile gameProfile)
-    {
+
+    private static GameProfile fixSkin(GameProfile gameProfile) {
         if (!gameProfile.getProperties().containsKey("texture"))
             return TileEntitySkull.updateGameProfile(gameProfile);
         else
             return gameProfile;
     }
-    
+
     @Override
-    public void onKillCommand()
-    {
+    public void awardKillScore(Entity p_191956_1_, int p_191956_2_, DamageSource p_191956_3_) {
+        //NO OP - Performance
+    }
+
+    @Override
+    public void onKillCommand() {
         logout();
     }
-    
+
     @Override
-    public void onUpdate()
-    {
+    public void onUpdate() {
+        this.openContainer = DUMMY_CONTAINER;
         super.onUpdate();
         this.onUpdateEntity();
         this.playerMoved();
     }
-    
+
     @Override
-    public void onDeath(DamageSource cause)
-    {
+    public void onDeath(DamageSource cause) {
         super.onDeath(cause);
         logout();
     }
-    
+
     private void logout() {
         this.dismountRidingEntity();
         getServer().getPlayerList().playerLoggedOut(this);
     }
-    
-    private void playerMoved()
-    {
-        if (posX != lastReportedPosX || posY != lastReportedPosY || posZ != lastReportedPosZ)
-        {
+
+    private void playerMoved() {
+        if (posX != lastReportedPosX || posY != lastReportedPosY || posZ != lastReportedPosZ) {
             server.getPlayerList().serverUpdateMovingPlayer(this);
             lastReportedPosX = posX;
             lastReportedPosY = posY;
             lastReportedPosZ = posZ;
         }
     }
-    
-    public void setSetPosition(double x, double y, double z, float yaw, float pitch)
-    {
+
+    public void setSetPosition(double x, double y, double z, float yaw, float pitch) {
         this.setX = x;
         this.setY = y;
         this.setZ = z;
         this.setYaw = yaw;
         this.setPitch = pitch;
     }
-    
-    public void resetToSetPosition()
-    {
+
+    public void resetToSetPosition() {
         setLocationAndAngles(setX, setY, setZ, setYaw, setPitch);
+    }
+
+    @Nonnull
+    @Override
+    public String getPlayerIP() {
+        return "1.3.3.7";
     }
 }
--- src/main/java/carpet/forge/patches/NetworkManagerFake.java
+++ src/main/java/carpet/forge/patches/NetworkManagerFake.java
@@ -1,13 +1,19 @@
 package carpet.forge.patches;
 
 import io.netty.channel.Channel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.EnumPacketDirection;
 import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
 public class NetworkManagerFake extends NetworkManager
 {
     private final Channel channel;
-    
+
     public NetworkManagerFake(EnumPacketDirection p)
     {
         super(p);
@@ -15,28 +21,31 @@ public class NetworkManagerFake extends NetworkManager
     }
 
     @Override
-    public void sendPacket(Packet<?> packetIn) 
-    {
+    public void sendPacket(Packet<?> packetIn) {
     }
 
     @Override
-    public void sendPacket(Packet<?> packetIn, GenericFutureListener<? extends Future<? super Void>> listener, GenericFutureListener<? extends Future<? super Void>>... listeners) 
-    {
+    public void sendPacket(Packet<?> packetIn, GenericFutureListener<? extends Future<? super Void>> listener, GenericFutureListener<? extends Future<? super Void>>... listeners) {
     }
 
     @Override
     public void disableAutoRead()
     {
     }
-    
+
     @Override
     public void handleDisconnection()
     {
     }
-    
+
     @Override
     public Channel channel()
     {
         return this.channel;
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        return new InetSocketAddress("localhost", 1337);
     }
 }
